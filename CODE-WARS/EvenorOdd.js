b73b6d63655be185b3944bb5d5a183a8223b006e
Berry Bash <mubashar.nuru@gmail.com>
2024-03-18 06:40:38 +0100

    katas

---
+++ CODE-WARS/EvenorOdd.js
@@ -0,0 +1,20 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0){
+       return "Even"
+    } else {
+       return "Odd"
+    }
+}
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  }
+
+  function evenOrOdd(number) {
+    if(number%2==0){
+        return "Even"
+    }
+   return "Odd"
+  
+}
+console.log(evenOrOdd(40));
---
+++ CODE-WARS/stringtonumber.js
@@ -0,0 +1,26 @@
+// This converts string to Number
+const stringToNumber = function(str) {
+    return Number(str);
+  }
+const stringToNumberr = function(str) {
+    return +(str);
+  }
+
+  let stringValue = "123";
+let numberValue = parseInt(stringValue, 10); // The second argument is the radix. For decimal numbers, it's 10.
+console.log(numberValue); // Output: 123
+
+
+//   This converts number to a string
+const numberToString = function(num) {
+    return num.toString();
+};
+
+const numberToStringg = function(num) {
+    return `${num}`;
+  }; //You can also use template literal
+  
+
+ 
+ 
+  
